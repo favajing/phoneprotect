2a6a7db4f8f50152b3fa480fb86d3dbb58422ca3
fjj <fdf@132.com>
2016-05-06 22:59:54 +0800

    Widgets

---
+++ app/src/main/java/com/fjj/phoneprotect/engine/MyWidget.java
@@ -0,0 +1,9 @@
+package com.fjj.phoneprotect.engine;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by favaj on 2016/5/6.
+ */
+public class MyWidget extends AppWidgetProvider {
+}
