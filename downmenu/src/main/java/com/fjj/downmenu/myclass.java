039d43cf7371985ca3175ad894afead1ec353c95
favajing <favajing@163.com>
2016-05-17 14:48:50 +0800

    svn

---
+++ downmenu/src/main/java/com/fjj/downmenu/myclass.java
@@ -0,0 +1,16 @@
+package com.fjj.downmenu;
+
+/**
+ * @项目名: mobilesafe
+ * @包名: com.fjj.downmenu
+ * @创建者: 范晶晶
+ * @创建时间: 2016/5/17 14:48
+ * @描述 TODO
+ * @svn版本:
+ * @更新人:
+ * @更新时间:
+ * @更新描述:
+ */
+
+public class myclass {
+}
