dc4b4c4de3402fe8daa5dbf586d43350a102e83c
favajing <favajing@163.com>
2016-05-17 14:51:08 +0800

    svn

--- downmenu/src/main/java/com/fjj/downmenu/myClass2.java
+++ downmenu/src/main/java/com/fjj/downmenu/myClass2.java
@@ -4,13 +4,13 @@ package com.fjj.downmenu;
  * @项目名: mobilesafe
  * @包名: com.fjj.downmenu
  * @创建者: 范晶晶
- * @创建时间: 2016/5/17 14:48
+ * @创建时间: 2016/5/17 14:50
  * @描述 TODO
- * @svn版本:
- * @更新人:
- * @更新时间:
+ * @svn版本: $$Rev$$
+ * @更新人: $$Author$$
+ * @更新时间: $$Date$$
  * @更新描述:
  */
 
-public class myclass {
+public class myClass2 {
 }
